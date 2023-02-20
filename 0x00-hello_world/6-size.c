c8fe176f70064143b103beceffbed7d206db51a9
tasnimo123 <soma123580@gmail.com>
2023-02-20 13:11:02 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)", sizeof(char));
+	printf("size of a int: %d byte(s)", sizeof(int));
+	printf("size of a long int: %d byte(s)", sizeof(long int));
+	printf("size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("size of a floet: %d byte(s)", sizeof(float));
+	return (0);
+} 	
