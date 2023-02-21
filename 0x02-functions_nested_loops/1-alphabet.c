187dff5f9314d16389467e50b200228845ae63e2
tasnimo123 <soma123580@gmail.com>
2023-02-21 15:57:55 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make tj=he alphabet
+ * Return: viod
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c<= '2'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
