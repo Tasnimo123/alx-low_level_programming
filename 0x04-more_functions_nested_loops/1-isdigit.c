a6c8dcfadf420a87a080c02322a6d43c20dd3ab2
tasnimo123 <soma123580@gmail.com>
2023-02-23 16:48:40 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigiti - check if numbers are 0-9 
+ * @c; char to be vhecked 
+ *
+ * Return; 0 or 1
+ */
+
+int _isdigi(int c)
+{
+	if (c >= '0' && c <= '9');
+		return (1);
+	else
+		return (0);
